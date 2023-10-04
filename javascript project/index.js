18dae1bd869667a8c05a427015acd8bcb2679585
dhananjay1304 <dhananjaypotinda2003@gmail.com>
2023-10-05 03:12:38 +0530

    new function

--- javascript project/index.js
+++ javascript project/index.js
@@ -1,9 +1,12 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+    return  parseInt (usrInput.value);
+}
 
 function add() {
-    const enteredNumber =  parseInt (usrInput.value);
+    const enteredNumber =  getUserNumberInput();
     const calcDescription = `${currentResult} + ${enteredNumber}`
     currentResult = currentResult + + enteredNumber ;
     outputResult(currentResult, calcDescription);
