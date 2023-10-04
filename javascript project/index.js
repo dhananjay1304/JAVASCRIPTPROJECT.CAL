688ad4180fc3fa3754deb0c8c92ac41d701620e0
dhananjay1304 <dhananjaypotinda2003@gmail.com>
2023-10-04 15:01:42 +0530

    small chng in fun

--- javascript project/index.js
+++ javascript project/index.js
@@ -2,13 +2,11 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 
-function add(num1, num2) {
-    const result = num1 + num2;
-    return result;
+function add() {
+    currentResult = currentResult + userInput.value ;
+    outputResult(currentResult, '');
 }
-
-add();
 
 addBtn.addEventListener('click', add);
 
-outputResult(currentResult, calculationDescription);
+outputResult(currentResult);
