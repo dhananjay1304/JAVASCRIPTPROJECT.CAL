62781298f915df8d600ad6a950c1bbde7302fbf7
dhananjay1304 <dhananjaypotinda2003@gmail.com>
2023-10-05 03:27:30 +0530

    blast

--- javascript project/index.js
+++ javascript project/index.js
@@ -5,12 +5,28 @@ function getUserNumberInput() {
     return  parseInt (usrInput.value);
 }
 
+function createAndWriteOutput(operator,resultBeforeCalc,calcNumber) {
+    const calcDescription = `${currentResult} ${operator} ${enteredNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
 function add() {
     const enteredNumber =  getUserNumberInput();
     const calcDescription = `${currentResult} + ${enteredNumber}`
-    currentResult = currentResult + + enteredNumber ;
+    currentResult = currentResult + enteredNumber ;
     outputResult(currentResult, calcDescription);
 }
+
+function subtract() {
+    const enteredNumber =  getUserNumberInput();
+    const calcDescription = `${currentResult} - ${enteredNumber}`
+    currentResult = currentResult - enteredNumber ;
+    outputResult(currentResult, calcDescription);
+}
+
+function multiply() {}
+
+function divide() {}
 
 
 addBtn.addEventListener('click', add);
