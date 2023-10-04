6e5c9d8e2e8059a1fb0ab697142e3f281f164c8c
dhananjay1304 <dhananjaypotinda2003@gmail.com>
2023-10-04 14:42:40 +0530

    add eventlistner

--- javascript project/index.js
+++ javascript project/index.js
@@ -1,17 +1,14 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+
 function add(num1, num2) {
     const result = num1 + num2;
-    alert('The result is' + result);
+    return result;
 }
 
-add(1, 2);
-add(5, 5);
+add();
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
-
-let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`;
+addBtn.addEventListener('click', add);
 
 outputResult(currentResult, calculationDescription);
-
